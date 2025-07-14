a149181296889cf5973cf1b7354d2bf94eb44500
Eduard Marbach <mail@eduard-marbach.de>
2025-07-14 23:34:55 +0200

    chore: update

--- README.md
+++ README.md
@@ -15,5 +15,20 @@ Checkout the `update.sh` and modify the version you want to create the matching 
 Also maybe double check what kind of files have changed.
 The most things should be done in the script like copy vendor out, rename things, clean up `go.mod` and `modules.txt`.
 
-Afterwards create a matching tag to be used in the pulumi provider.
-Then continue there.
+Checkout the desired remote tag (you should be on the tag and detached head).
+We are running the update script directly.
+
+`curl -sSL https://raw.githubusercontent.com/BlackDark/terraform-provider-f5os/refs/heads/main/update.sh | bash`
+
+Then we commit and deleteAfterwards create a matching tag to be used in the pulumi provider.
+The we need to prepare the new tag:
+
+```bash
+git commit -m "refactor: update for pulumi"
+git tag -d v1.8.0 # your used tag
+git tag v1.8.0
+git push origin v1.8.0
+```
+
+Done. 
+Continue in the pulumi provider.
--- update.sh
+++ update.sh
@@ -3,10 +3,12 @@
 set -e
 
 # Define variables
-VERSION="v1.8.0"
+VERSION=$(git describe --tags --abbrev=0)
 BASE_URL="https://github.com/F5Networks/terraform-provider-f5os/archive/refs/tags"
 TARBALL="${VERSION}.tar.gz"
 EXTRACT_DIR="upstream"
+
+echo "Checking out release version: $VERSION"
 
 # Download the tarball if not already downloaded
 if [ ! -f "$TARBALL" ]; then
@@ -21,23 +23,36 @@ echo "Extracting $TARBALL to $EXTRACT_DIR/ ..."
 mkdir -p "$EXTRACT_DIR"
 tar -xzf "$TARBALL" -C "$EXTRACT_DIR" --strip-components=1
 
-# Replace directories and perform text substitutions
-echo "Replacing vendor directory with upstream contents..."
-rm -rf vendor
-mv "$EXTRACT_DIR/vendor" .
-rm -rf f5osclient
+# remove all current files and replace with new ones
+(git rm -r .) || true
+(
+    cd ${EXTRACT_DIR}
+    rsync -a --remove-source-files ./ ../
+)
+
+# # Replace directories and perform text substitutions
+# echo "Replacing vendor directory with upstream contents..."
+# rm -rf vendor
+# mv "$EXTRACT_DIR/vendor" .
+# rm -rf f5osclient
+# rm -rf f5os
 mv vendor/gitswarm.f5net.com/terraform-providers/f5osclient .
-rm -rf f5os
-mv "$EXTRACT_DIR/internal/provider" f5os
+#mv "${EXTRACT_DIR}/internal/provider" f5os
+mv "internal/provider" f5os
+
+# # Remove and restore docs and examples
+# rm -rf docs examples
+# mv "$EXTRACT_DIR/examples" .
+# mv "$EXTRACT_DIR/docs" .
 
 # Replace package names in source files
 find f5os -type f -exec sed -i '' 's/package provider/package f5os/g' {} +
 find f5osclient -type f -exec sed -i '' 's/package f5os/package f5osclient/g' {} +
 
-# Remove and restore docs and examples
-rm -rf docs examples
-mv "$EXTRACT_DIR/examples" .
-mv "$EXTRACT_DIR/docs" .
+find . -type f -name 'go.mod' -exec sed -i '' 's|gitswarm.f5net.com/terraform-providers/terraform-provider-f5os|github.com/BlackDark/terraform-provider-f5os|g' {} +
+sed -i '' '/gitswarm\.f5net\.com\/terraform-providers\/f5osclient/d' go.mod
+sed -i '' 's|"gitswarm.f5net.com/terraform-providers/terraform-provider-f5os/internal/provider"|provider "github.com/BlackDark/terraform-provider-f5os/f5os"|g' main.go
+
 
 # Remove 3 lines starting from "# gitswarm" in vendor/modules.txt
 sed -i '' '/^# gitswarm/{N;N;d;}' vendor/modules.txt
@@ -55,9 +70,17 @@ find f5os -type f -exec sed -i '' 's/"gitswarm\.f5net\.com\/terraform-providers\
 gofmt -w f5osclient
 gofmt -w f5os
 
+go mod tidy
+
 # Clean up downloaded tarball and extracted files
 echo "Cleaning up..."
 rm -rf "$TARBALL" "$EXTRACT_DIR"
 rm -rf upstream
+rm -rf internal
+rm -rf vendor/gitswarm.f5net.com
 
-echo "All steps completed."
+git add .
+
+echo "All steps completed. Check the changes commit and retag with ${VERSION}."
+
+
